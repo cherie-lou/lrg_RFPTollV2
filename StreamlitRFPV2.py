0e7a4ac297b39e925bf021962d124e9158e2b107
cherie-lou <149835051+cherie-lou@users.noreply.github.com>
2023-12-08 16:23:36 -0600

    Update StreamlitRFPV2.py

--- StreamlitRFPV2.py
+++ StreamlitRFPV2.py
@@ -623,28 +623,28 @@ st.header('RfpLoad Query Generator')
 # connection_string="DRIVER="+__DRIVER__+';SERVER='+__SERVER__+';PORT=1443;DATABASE='+__DATABASE__+';AUTHENTICATION=ActiveDirectoryPassword'+';Encrypt=yes'+';UID='+__USER__+';PWD='+__PASS__
 # connection = pyodbc.connect(connection_string)
 
-@st.cache_resource
-def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["SERVER"]
-        + ";DATABASE="
-        + st.secrets["DATABASE"]
-        + ";UID="
-        + st.secrets["USER"]
-        + ";PWD="
-        + st.secrets["PASS"]
-    )
+# @st.cache_resource
+# def init_connection():
+#     return pyodbc.connect(
+#         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+#         + st.secrets["SERVER"]
+#         + ";DATABASE="
+#         + st.secrets["DATABASE"]
+#         + ";UID="
+#         + st.secrets["USER"]
+#         + ";PWD="
+#         + st.secrets["PASS"]
+#     )
 
-conn = init_connection()
+# conn = init_connection()
 
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-# @st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+# # Perform query.
+# # Uses st.cache_data to only rerun when the query changes or after 10 min.
+# # @st.cache_data(ttl=600)
+# def run_query(query):
+#     with conn.cursor() as cur:
+#         cur.execute(query)
+#         return cur.fetchall()
 
 
 start_date = st.date_input('Start Date', value=None)
